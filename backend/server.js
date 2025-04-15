a4d5118d360c20482034b99ce68a7279dde5e83b
Sk13-Satyam <cloudsk13new@gmail.com>
2025-04-15 14:20:19 +0530

    authControllersAdded02

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,20 @@ import express from 'express';
 import authRoutes from './routes/auth.routes.js';
 import dotenv from 'dotenv';
 import connectMongoDB from './db/connectMongoDB.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //to parse form data(urlencoded data)
+app.use(cookieParser()); //to parse cookies
 
 // console.log(process.env.MONGO_URI)
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     // Connect to MongoDB
     connectMongoDB()
-        
-} );
+
+});
