5511d6d8dbdaf25ab7ee7292957447d808d0aafb
Sk13-Satyam <cloudsk13new@gmail.com>
2025-04-17 15:24:19 +0530

    UI desgin completed11

---
+++ frontend/src/utils/db/dummy.js
@@ -0,0 +1,116 @@
+export const POSTS = [
+	{
+		_id: "1",
+		text: "here we begin our journey of learning....!!",
+		img: "/posts/post1.jpg",
+		user: {
+			username: "Sarthak",
+			profileImg: "/avatars/boy1.png",
+			fullName: "Satyam Kesharwani",
+		},
+		comments: [
+			{
+				_id: "1",
+				text: "Nice Tutorial",
+				user: {
+					username: "Samk",
+					profileImg: "/avatars/girl1.png",
+					fullName: "Sam Kesharwani",
+				},
+			},
+		],
+		likes: ["6658s891", "6658s892", "6658s893", "6658s894"],
+	},
+	{
+		_id: "2",
+		text: "How you guys doing? 😊",
+		user: {
+			username: "Sarthak",
+			profileImg: "/avatars/boy2.png",
+			fullName: "Satyam Kesharwani",
+		},
+		comments: [
+			{
+				_id: "1",
+				text: "Nice Tutorial",
+				user: {
+					username: "Samk",
+					profileImg: "/avatars/girl2.png",
+					fullName: "Sam Kesharwani",
+				},
+			},
+		],
+		likes: ["6658s891", "6658s892", "6658s893", "6658s894"],
+	},
+	{
+		_id: "3",
+		text: "Planning for the next project. Any suggestions?",
+		img: "/posts/post2.jpg",
+		user: {
+			username: "Sarthak",
+			profileImg: "/avatars/boy3.png",
+			fullName: "Satyam Kesharwani",
+		},
+		comments: [],
+		likes: ["6658s891", "6658s892", "6658s893", "6658s894", "6658s895", "6658s896"],
+	},
+	{
+		_id: "4",
+		text: "I'm about to master the art of coding. Any tips?",
+		img: "/posts/post3.jpg",
+		user: {
+			username: "Sarthak",
+			profileImg: "/avatars/boy3.png",
+			fullName: "Satyam Kesharwani",
+		},
+		comments: [
+			{
+				_id: "1",
+				text: "Nice Tutorial",
+				user: {
+					username: "Samk",
+					profileImg: "/avatars/girl3.png",
+					fullName: "Sam Kesharwani",
+				},
+			},
+		],
+		likes: [
+			"6658s891",
+			"6658s892",
+			"6658s893",
+			"6658s894",
+			"6658s895",
+			"6658s896",
+			"6658s897",
+			"6658s898",
+			"6658s899",
+		],
+	},
+];
+
+export const USERS_FOR_RIGHT_PANEL = [
+	{
+		_id: "1",
+		fullName: "Satyam Kesharwani",
+		username: "satyam07",
+		profileImg: "/avatars/boy2.png",
+	},
+	{
+		_id: "2",
+		fullName: "Sam Kesharwani",
+		username: "Samk",
+		profileImg: "/avatars/girl1.png",
+	},
+	{
+		_id: "3",
+		fullName: "Bob Kesharwani",
+		username: "bobdoe",
+		profileImg: "/avatars/boy3.png",
+	},
+	{
+		_id: "4",
+		fullName: "Daisy Kesharwani",
+		username: "daisydoe",
+		profileImg: "/avatars/girl2.png",
+	},
+];
