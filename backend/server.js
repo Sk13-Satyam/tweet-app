bb85470f63e96f9d60c866da1e15eff9a9407794
Sk13-Satyam <cloudsk13new@gmail.com>
2025-04-20 12:11:34 +0530

    image post functionality added16

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,24 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import {v2 as cloudinary} from 'cloudinary';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
-import authRoutes from './routes/auth.route.js';
-import userRoutes from './routes/user.route.js';
-import postRoutes from './routes/post.route.js';
-import notificationRoutes from './routes/notification.route.js';
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
-import connectMongoDB from './db/connectMongoDB.js';
+import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(express.json());
+app.use(express.json({ limit: "5mb" })); //to parse json data
 app.use(express.urlencoded({ extended: true })); //to parse form data(urlencoded data)
 app.use(cookieParser()); //to parse cookies
 
@@ -26,12 +26,10 @@ app.use(cookieParser()); //to parse cookies
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
-app.use("/api/notification",notificationRoutes);
-
+app.use("/api/notification", notificationRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    // Connect to MongoDB
-    connectMongoDB()
-
+  console.log(`Server is running on port ${PORT}`);
+  // Connect to MongoDB
+  connectMongoDB();
 });
