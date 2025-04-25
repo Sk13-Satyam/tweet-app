acb067596a8cae09280fc0ce9a1c03e377e0d0ba
Sk13-Satyam <cloudsk13new@gmail.com>
2025-04-25 20:20:36 +0530

    prepared app for the deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -9,7 +10,9 @@ import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
+
 dotenv.config();
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -18,18 +21,28 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(express.json({ limit: "5mb" })); //to parse json data
-app.use(express.urlencoded({ extended: true })); //to parse form data(urlencoded data)
-app.use(cookieParser()); //to parse cookies
+const __dirname = path.resolve();
 
-// console.log(process.env.MONGO_URI)
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/notification", notificationRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  // Connect to MongoDB
   connectMongoDB();
 });
